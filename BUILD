3a6711305810a079ca1d5ecf1fa0a403b0389c34
Warriorstar Orion <orion@snowfrost.garden>
2020-12-22 12:36:03 -0500

    Initial commit

---
+++ BUILD
@@ -0,0 +1,8 @@
+load("@io_bazel_rules_dotnet//dotnet:defs.bzl", "core_binary")
+
+core_binary(
+    name = "a.exe",
+    srcs = [
+        "a.cs",
+    ],
+)
---
+++ a.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace example_tool
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+          Console.WriteLine("ok");
+        }
+    }
+}
